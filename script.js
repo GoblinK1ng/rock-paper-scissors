3d4b65a038cee622aeb77dc9237e7b6cae3cfe95
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2024-07-03 19:09:43 -0400

    restored implemntation of html and src files

---
+++ script.js
@@ -0,0 +1,42 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let choice = Math.random();
+    if (choice < 1/3) return "rock";
+    else if (choice > 2/3) return "paper";
+    else return "scissors";
+}
+
+function getHumanChoice(){
+    let choice = prompt("Enter either Rock, Paper or Scissors");
+    if (choice.toLowerCase() === "rock") return "rock";
+    else if (choice.toLowerCase() === "paper") return "paper";
+    else if (choice.toLowerCase()  === "scissors") return "Scissors";
+    else return "INVALID";
+}
+
+function playRound(humanChoice, computerChoice){
+    if ((humanChoice === "rock" && computerChoice === "paper") || 
+    (humanChoice === "paper" && computerChoice === "scissors") || 
+    (humanChoice === "scissors" && computerChoice === "rock")){
+        console.log("You Lose!",computerChoice,"beats",humanChoice);
+        computerScore++;
+    }
+    else if ((humanChoice === "paper" && computerChoice === "rock") || 
+    (humanChoice === "sicssors" && computerChoice === "paper") || 
+    (humanChoice === "rock" && computerChoice === "scissors")){
+        console.log("You Win!",humanChoice,"beats",computerChoice);
+        humanScore++;
+    }
+    else console.log("Its a Tie");
+}
+
+function playGame(amount){
+    for (let i = 0; i <amount;i++){
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+    console.log(humanScore,computerScore);
+}
+
+playGame(5);
